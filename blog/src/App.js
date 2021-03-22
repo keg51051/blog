437f9da0a1d5ee13a4bf7295f0b04429fc51283d
Hanseong Kim <keg51051@gmail.com>
2021-03-22 21:14:28 +0900

    first day

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,10 +3,21 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
 
   let [title, changeTitle] = useState(['남자 코트 추천', '고기 맛집', '모바일 게임 추천']);
   let [like, changeLike] = useState(0);
+
+  // function 제목바꾸기() {
+  //   // Deep copy the title array
+  //   var newArray = [...title];
+  //   // Alphabetically sort
+  //   newArray.sort(function (a,b) {
+  //     return a.localeCompare(b);
+  //   });
+  //   changeTitle(newArray);
+  // }
 
   return (
     <body>
@@ -14,7 +25,7 @@ function App() {
       <div className="black-nav">
         <div>Blog</div>
       </div>
-      <button onClick={ ()=>{changeTitle(['여자 코트 추천', '고기 맛집', '모바일 게임 추천'])}}>button</button>
+      {/* <button onClick={ 제목바꾸기 }>button</button> */}
       <div className="list">
         <h3> { title[0] } <span onClick={ ()=>{changeLike(like + 1)} }>👍</span> {like} </h3>
         <p>3월 22일</p>
@@ -30,9 +41,22 @@ function App() {
         <p>3월 24일</p>
         <hr/>
       </div>
+
+      <Modal></Modal>
+
     </div>
     </body>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+        <h2>title</h2>
+        <p>date</p>
+        <p>details</p>
+    </div>
+  )
 }
 
 export default App;
