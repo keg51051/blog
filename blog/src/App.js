466f33149658319ce8ac8d1e2238ecd18ce27112
Hanseong Kim <keg51051@gmail.com>
2021-03-22 19:21:57 +0900

    start

---
+++ blog/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let posts = '고기 맛집';
+  return (
+    <body>
+    <div className="App">
+      <div className="black-nav">
+        <div>한성 Blog</div>
+      </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
