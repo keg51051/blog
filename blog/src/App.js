e2fa9ee5d22840edb82fc61f7d41cd25005a9ee3
Hanseong Kim <keg51051@gmail.com>
2021-03-22 20:33:07 +0900

    EventListener

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
   let [title, changeTitle] = useState(['남자 코트 추천', '고기 맛집', '모바일 게임 추천']);
+  let [like, changeLike] = useState(0);
 
   return (
     <body>
@@ -12,8 +14,9 @@ function App() {
       <div className="black-nav">
         <div>Blog</div>
       </div>
+      <button onClick={ ()=>{changeTitle(['여자 코트 추천', '고기 맛집', '모바일 게임 추천'])}}>button</button>
       <div className="list">
-        <h3> { title[0] } </h3>
+        <h3> { title[0] } <span onClick={ ()=>{changeLike(like + 1)} }>👍</span> {like} </h3>
         <p>3월 22일</p>
         <hr/>
       </div>
