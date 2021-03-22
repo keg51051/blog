02ba8a5836e7e29301b8e9be2f93686da549f3aa
Hanseong Kim <keg51051@gmail.com>
2021-03-22 20:14:43 +0900

    posting list

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '고기 맛집';
+
+  let [title, changeTitle] = useState(['남자 코트 추천', '고기 맛집', '모바일 게임 추천']);
+
   return (
     <body>
     <div className="App">
       <div className="black-nav">
-        <div>한성 Blog</div>
+        <div>Blog</div>
       </div>
       <div className="list">
-        <h3> { posts } </h3>
+        <h3> { title[0] } </h3>
         <p>3월 22일</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[1] } </h3>
+        <p>3월 23일</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[2] } </h3>
+        <p>3월 24일</p>
         <hr/>
       </div>
     </div>
